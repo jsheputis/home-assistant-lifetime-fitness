d4ae06227c3320e8511910eb2020ade4237b984e
James Sheputis <jsheputis@users.noreply.github.com>
2025-12-07 16:36:50 -0600

    Fix test

--- custom_components/lifetime_fitness/config_flow.py
+++ custom_components/lifetime_fitness/config_flow.py
@@ -104,7 +104,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Handle the initial step."""
         if user_input is None:
-            return self.async_show_form(step_id="user", data_schema=STEP_USER_DATA_SCHEMA)
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors={}
+            )
 
         errors = {}
 
